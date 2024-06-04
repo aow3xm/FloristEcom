252bee93db1f0efcb649a81bef699b77aa63872f
aow3xm <149279753+aow3xm@users.noreply.github.com>
2024-06-04 22:58:05 +0700

    feat: Add MailService for sending reset password emails

---
+++ server/src/modules/mail/mail.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import * as sgMail from '@sendgrid/mail';
+
+@Injectable()
+export class MailService {
+  constructor() {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  }
+
+  async sendResetPasswordEmail(to: string, token: string): Promise<void> {
+    const msg = {
+      to,
+      from: process.env.SENDGRID_FROM,
+      templateId: 'd-52d76971b1214cc69badd0ddf21082fc',
+      dynamic_template_data: {
+        reset_password_url: `http://your-frontend-url/reset-password?token=${token}`,
+      },
+    };
+
+    try {
+      await sgMail.send(msg);
+      console.log('Reset password email sent successfully');
+    } catch (error) {
+      console.error('Error sending reset password email', error);
+    }
+  }
+}
