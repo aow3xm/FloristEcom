7d6d47573287d543c03b3222b547eed388ee5059
aow3xm <149279753+aow3xm@users.noreply.github.com>
2024-06-05 19:44:54 +0700

    feat: Add Collection module and update import paths

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { CollectionModule } from './modules/collection/collection.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ConfigModule } from '@nestjs/config';
     }),
     AuthModule,
     UserModule,
+    CollectionModule,
   ],
 })
 export class AppModule {}
